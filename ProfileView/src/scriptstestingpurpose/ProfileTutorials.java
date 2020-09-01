bc410224dbb9b10c7ebdfb81798c9ea86ba7b7f6
Administrator <Administrator@Admin-PC>
2020-09-01 12:12:20 +0530

    Added  Screenshots.screenshot(); method in Tutorial section on 01-09-2020 12:11 PM

--- ProfileView/src/scriptstestingpurpose/ProfileTutorials.java
+++ ProfileView/src/scriptstestingpurpose/ProfileTutorials.java
@@ -1,23 +1,26 @@
 package scriptstestingpurpose;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 import basicOperations.ActionClass;
 import basicOperations.LoginOperations;
+import basicOperations.Screenshots;
 import pom.Tutorials;
  
 public class ProfileTutorials extends LoginOperations
 
 {
 	@Test
-	public static void ProfileVideoTutorials() throws InterruptedException
+	public static void ProfileVideoTutorials() throws InterruptedException, IOException
 	{
 		
 		ActionClass.ActionClassHome();
 		Tutorials vt = new Tutorials(driver);
 		vt.TutorialVideo();
  		ActionClass.ActionClassVideo();
-		 
+		 Screenshots.screenshot();
 	}
  
 }
